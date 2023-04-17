1fa12d0acbbe85278136fea6ba241c016fc3df5f
Matheus Tanaka <matheustanakanog@gmail.com>
2023-04-17 15:15:18 -0300

    feat: add models and create instances

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("aprendendo orientacão obejto");
+        curso1.setCargaHoraria(10);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de Javascript");
+        curso2.setDescricao("aprendendo programacao funcional");
+        curso2.setCargaHoraria(10);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("mentoria de java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
     }
 }
