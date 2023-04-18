6fa50ba27a07f6f5705de21461dd6d81a5de67a1
Matheus Tanaka <matheustanakanog@gmail.com>
2023-04-17 22:20:24 -0300

    feat: challenge finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -16,17 +14,49 @@ public class Main {
         curso2.setDescricao("aprendendo programacao funcional");
         curso2.setCargaHoraria(10);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
 
         Mentoria mentoria1 = new Mentoria();
         mentoria1.setTitulo("Mentoria de Java");
         mentoria1.setDescricao("mentoria de java");
         mentoria1.setData(LocalDate.now());
 
-        System.out.println(mentoria1);
+//        System.out.println(mentoria1);
 
-        Conteudo conteudo1 = new Curso();
-        Conteudo conteudo2 = new Mentoria();
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Java developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devMatheus = new Dev();
+
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Matheus - Conteúdo inscritos: " + devMatheus.getConteudosInscritos());
+
+        devMatheus.progredir(); // progredir = concluir o conteudo
+        // Analisando como ficou os conteúdos inscritos depois de serem progredidos
+        System.out.println("Matheus - Conteúdo inscritos: " + devMatheus.getConteudosInscritos());
+        System.out.println("Matheus - Conteúdo concluidos: " + devMatheus.getConteudosConcluidos());
+        System.out.println("XP: " + devMatheus.calcularTotalXp());
+
+        Dev devJoao = new Dev();
+
+        devJoao.setNome("Joao");
+        devJoao.inscreverBootcamp(bootcamp);
+        System.out.println("João - Conteúdo inscritos: " + devJoao.getConteudosInscritos());
+
+        devJoao.progredir();
+        devJoao.progredir();
+        // Analisando como ficou os conteúdos inscritos depois de serem progredidos
+        System.out.println("João - Conteúdo inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("João- Conteúdo concluidos: " + devMatheus.getConteudosConcluidos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
+
+
+
     }
 }
